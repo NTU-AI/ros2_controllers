84bddd4e5605b5d5450fde51586cda86548d94a4
GilmarCorreia <gilmarcorreiajeronimo@gmail.com>
2022-09-06 10:23:26 -0300

    alterações conforme o fórum ros2_controllers

--- joint_state_broadcaster/src/joint_state_broadcaster.cpp
+++ joint_state_broadcaster/src/joint_state_broadcaster.cpp
@@ -131,6 +131,7 @@ JointStateBroadcaster::on_activate(const rclcpp_lifecycle::State & /*previous_st
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 JointStateBroadcaster::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
 {
+  joint_names_.clear();
   return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
